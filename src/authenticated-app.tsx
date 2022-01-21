16a321b716e887a3be75f0923ba5632fe890171b
kakachake <282281884@qq.com>
2022-01-21 11:01:33 +0800

    布局

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,3 +1,5 @@
+import styled from "@emotion/styled";
+import { Row } from "./components/lib";
 import { useAuth } from "./context/auth-context";
 import { ProjectListScreen } from "./screens/project-list";
 
@@ -5,8 +7,28 @@ export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
     <div>
-      <button onClick={logout}>登出</button>
-      <ProjectListScreen />
+      <PageHeader between={true}>
+        <HeaderLeft gap={true}>
+          <h3>Logo</h3>
+          <h3>项目</h3>
+          <h3>用户</h3>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>登出</button>
+        </HeaderRight>
+      </PageHeader>
+      <Main>
+        <ProjectListScreen />
+      </Main>
     </div>
   );
 };
+
+const PageHeader = styled(Row)``;
+
+const HeaderLeft = styled(Row)``;
+const HeaderRight = styled.div``;
+
+const Main = styled.main`
+  height: calc(100vh - 6rem);
+`;
